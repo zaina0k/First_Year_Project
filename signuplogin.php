9a2ebbcf37c8819535e8700910d769edafefe7b5
Ente Wang <ente.wang@student.manchester.ac.uk>
2022-02-17 18:41:53 +0000

    Password Hash, get attributes from other tables

--- signuplogin.php
+++ signuplogin.php
@@ -11,6 +11,7 @@ session_start();
         $user_name = $_POST['user_name'];
         $password = $_POST['password'];
         
+
         if(!empty($user_name) && !empty($password) && !is_numeric(($user_name)))
         {
             //check from database
@@ -24,11 +25,11 @@ session_start();
     
                     $user_data = mysqli_fetch_assoc($result);
                     
-                    if($user_data['password'] === $password)
+                    if(password_verify( $password, $user_data['password']))
                     {
                         $_SESSION['user_id'] = $user_data['user_id'];
     
-                        header("Location: Map.php");
+                        header("Location: index.php");
                         die;
                     }
                 }
@@ -52,20 +53,32 @@ session_start();
         $new_user_name = $_POST['new_user_name'];
         $new_password = $_POST['new_password'];
 
+        $hash_password = password_hash($new_password, PASSWORD_DEFAULT);
+
         if(!empty($new_user_name) && !empty($new_password) && !is_numeric(($new_user_name)))
         {
+
+            $query = "select * from users where user_name = '$new_user_name' limit 1";
+            $result = mysqli_query($con, $query);
+
+            if($result && mysqli_num_rows($result) > 0)
+            {
+                echo  "<div class='echotext'>That user name already exists</div>";
+            }else{
+
             //save to database
             $user_id = random_num(20);
-            $query = "insert into users (user_id, user_name, password) values ('$user_id', '$new_user_name', '$new_password')";
+            $query = "insert into users (user_id, user_name, password) values ('$user_id', '$new_user_name', '$hash_password')";
             mysqli_query($con, $query);
             echo  "<div class='echotext'>You have successfully Signed Up! please click log in</div>";
             
-        }else
-        {
+                }
+        }
+
+       else {
             echo "<div class='echotext'>Please enter some valid information!</div>";
             
         }
-
     }
 
 ?>
