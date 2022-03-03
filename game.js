7f59e45998ca65a40c6a432590eb3cce99db4087
amaan025 <97348141+amaan025@users.noreply.github.com>
2022-03-03 08:16:53 +0000

    Update game.js

--- game.js
+++ game.js
@@ -14,9 +14,29 @@ var infect_chance = 0.1; //the chance every day that a new device is randomly in
 
 var upgrades_array=[0,0,0,0,0,0,0,0,0,0];
 
-function display_info(num){
-  if (num==0){
-    document.getElementById("achievement1").src = "images/img2.png";
+function changeImage1(){
+  var image1 = document.getElementById('achievement1');
+  if (image1.src.match("new_img1")) {
+    image1.src = "images/img1.png";
+  } else {
+    image1.src = "images/new_img1.png";
+  }
+
+}
+function changeImage2(){
+  var image2 = document.getElementById('achievement2');
+  if (image2.src.match("new_img2")) {
+    image2.src = "images/img2.png";
+  } else {
+    image2.src = "images/new_img2.png";
+  }
+}
+function changeImage3(){
+  var image3 = document.getElementById('achievement3');
+  if (image3.src.match("new_img3")) {
+    image3.src = "images/img3.png";
+  } else {
+    image3.src = "images/new_img3.png";
   }
 }
 
