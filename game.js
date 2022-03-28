bf4f73e7bab0a9e7453005217b6c8de1567edca9
barkol03 <berson500@gmail.com>
2022-03-28 14:59:25 +0100

    created Achievements.js

--- game.js
+++ game.js
@@ -127,32 +127,6 @@ function check_win(){
      }
 }
 
-function changeImage1(){
-  var image1 = document.getElementById('achievement1');
-  if (image1.src.match("new_img1")) {
-    image1.src = "images/img1.png";
-  } else {
-    image1.src = "images/new_img1.png";
-  }
-
-}
-function changeImage2(){
-  var image2 = document.getElementById('achievement2');
-  if (image2.src.match("new_img2")) {
-    image2.src = "images/img2.png";
-  } else {
-    image2.src = "images/new_img2.png";
-  }
-}
-function changeImage3(){
-  var image3 = document.getElementById('achievement3');
-  if (image3.src.match("new_img3")) {
-    image3.src = "images/img3.png";
-  } else {
-    image3.src = "images/new_img3.png";
-  }
-}
-
 function change_theme(theme){
   var styleEl = document.createElement("style");
   document.head.appendChild(styleEl);
